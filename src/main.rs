bc1ae494045476befc8ea0206101a19298acb7db
DiscreteTom <discrete_tom@outlook.com>
2024-08-21 02:30:18 +0000

    chore: apply shinkuro

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use axum::{extract::Query, http::StatusCode, routing::get, Router};
 use lambda_extension::{tracing, Extension};
+use shinkuro::Cache;
 use std::{
   collections::HashMap,
   env,
   net::{Ipv4Addr, SocketAddrV4},
 };
-use tokio::{fs, net::TcpListener};
+use tokio::net::TcpListener;
 
 async fn start_nacos_adapter() {
   let port = env::var("AWS_LAMBDA_NACOS_ADAPTER_PORT")
@@ -14,18 +15,26 @@ async fn start_nacos_adapter() {
     .unwrap_or(8848);
   let prefix =
     env::var("AWS_LAMBDA_NACOS_ADAPTER_CONFIG_PATH").unwrap_or("/mnt/efs/nacos".to_string());
+  let cache_size = env::var("AWS_LAMBDA_NACOS_ADAPTER_CACHE_SIZE")
+    .ok()
+    .and_then(|s| s.parse().ok())
+    .unwrap_or(64);
+
+  let cache = Cache::new(cache_size);
 
   let app = Router::new().route(
     "/nacos/v1/cs/configs",
     get(move |Query(params): Query<HashMap<String, String>>| {
       let group = params.get("group").unwrap();
       let data_id = params.get("dataId").unwrap();
-      let path = format!("{}/{}/{}", prefix, group, data_id);
+      let tenant = params.get("tenant").map(|s| s as &str).unwrap_or("");
+      let path = format!("{}/{}.{}.{}", prefix, tenant, group, data_id);
+      let mut cache = cache.clone();
       async move {
-        fs::read_to_string(&path)
-          .await
-          .map(|config| (StatusCode::OK, config))
-          .map_err(|e| (StatusCode::NOT_FOUND, e.to_string()))
+        match cache.get(path).await {
+          Ok(config) => (StatusCode::OK, (*config).clone()),
+          Err(e) => (StatusCode::NOT_FOUND, e.to_string()),
+        }
       }
     }),
   );
