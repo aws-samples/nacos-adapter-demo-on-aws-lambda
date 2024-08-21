05e1f1c0a015fdeb5b559360cbb84d1e6590d573
DiscreteTom <discrete_tom@outlook.com>
2024-08-21 03:13:36 +0000

    feat: support `/nacos/v2/cs/config`

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::{
   collections::HashMap,
   env,
   net::{Ipv4Addr, SocketAddrV4},
+  sync::Arc,
 };
 use tokio::net::TcpListener;
 
@@ -13,40 +14,46 @@ async fn start_nacos_adapter() {
     .ok()
     .and_then(|p| p.parse().ok())
     .unwrap_or(8848);
-  let prefix =
-    env::var("AWS_LAMBDA_NACOS_ADAPTER_CONFIG_PATH").unwrap_or("/mnt/efs/nacos/".to_string());
+  let prefix = Arc::new(
+    env::var("AWS_LAMBDA_NACOS_ADAPTER_CONFIG_PATH").unwrap_or("/mnt/efs/nacos/".to_string()),
+  );
   let cache_size = env::var("AWS_LAMBDA_NACOS_ADAPTER_CACHE_SIZE")
     .ok()
     .and_then(|s| s.parse().ok())
     .unwrap_or(64);
 
-  let mut cache = Cache::new(cache_size);
+  let cache = Cache::new(cache_size);
 
-  let app = Router::new().route(
-    "/nacos/v1/cs/configs",
-    get(
-      move |Query(params): Query<HashMap<String, String>>| async move {
-        let Some(group) = params.get("group").filter(|s| s.len() > 0) else {
-          return (StatusCode::BAD_REQUEST, "group is required".to_string());
-        };
-        let Some(data_id) = params.get("dataId").filter(|s| s.len() > 0) else {
-          return (StatusCode::BAD_REQUEST, "dataId is required".to_string());
-        };
+  let config_getter = |ns: &'static str| {
+    let prefix = prefix.clone();
+    let mut cache = cache.clone();
+    move |Query(params): Query<HashMap<String, String>>| async move {
+      let Some(group) = params.get("group").filter(|s| s.len() > 0) else {
+        return (StatusCode::BAD_REQUEST, "group is required".to_string());
+      };
+      let Some(data_id) = params.get("dataId").filter(|s| s.len() > 0) else {
+        return (StatusCode::BAD_REQUEST, "dataId is required".to_string());
+      };
 
-        let tenant = params
-          .get("tenant")
-          .filter(|s| s.len() > 0)
-          .map(|s| s as &str)
-          .unwrap_or("public");
-        let path = format!("{}{}/{}/{}", prefix, tenant, group, data_id);
+      // TODO: "tag" in nacos api v2 is not supported yet
 
-        match cache.get(path).await {
-          Ok(config) => (StatusCode::OK, (*config).clone()),
-          Err(e) => (StatusCode::NOT_FOUND, e.to_string()),
-        }
-      },
-    ),
-  );
+      let tenant = params
+        .get(ns)
+        .filter(|s| s.len() > 0)
+        .map(|s| s as &str)
+        .unwrap_or("public");
+      let path = format!("{}{}/{}/{}", prefix, tenant, group, data_id);
+
+      match cache.get(path).await {
+        Ok(config) => (StatusCode::OK, (*config).clone()),
+        Err(e) => (StatusCode::NOT_FOUND, e.to_string()),
+      }
+    }
+  };
+
+  let app = Router::new()
+    .route("/nacos/v1/cs/configs", get(config_getter("tenant")))
+    .route("/nacos/v2/cs/config", get(config_getter("namespaceId")));
 
   let listener = TcpListener::bind(SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), port))
     .await
