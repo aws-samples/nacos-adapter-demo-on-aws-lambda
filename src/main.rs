3c51966bf5d4002f72e7ce88b20c1623d7573ecd
DiscreteTom <discrete_tom@outlook.com>
2024-08-29 00:26:07 +0000

    feat: support config listener

--- src/main.rs
+++ src/main.rs
@@ -9,24 +9,30 @@ use crate::constant::{
 use axum::{
   body::Body,
   extract::Query,
-  http::{Request, StatusCode},
-  routing::{any, get},
+  http::{HeaderMap, Request, StatusCode},
+  routing::{any, get, post},
   Router,
 };
 use lambda_extension::{
+  service_fn,
   tracing::{self, error},
-  Extension,
+  Error, LambdaEvent, NextEvent,
 };
 use serde_json::json;
 use std::{
-  collections::HashMap,
+  collections::{HashMap, HashSet},
   env,
   net::{Ipv4Addr, SocketAddrV4},
   sync::Arc,
+  time::Duration,
 };
-use tokio::net::TcpListener;
+use tokio::{
+  net::TcpListener,
+  sync::{broadcast, mpsc},
+  time::sleep,
+};
 
-async fn start_nacos_adapter() {
+async fn start_nacos_adapter(mut refresh_rx: mpsc::Receiver<()>) {
   let port = env::var("AWS_LAMBDA_NACOS_ADAPTER_PORT")
     .ok()
     .and_then(|p| p.parse().ok())
@@ -39,7 +45,32 @@ async fn start_nacos_adapter() {
     .and_then(|s| s.parse().ok())
     .unwrap_or(64);
 
-  let mut cache = Cache::new(cache_size);
+  let cache = Cache::new(cache_size);
+
+  let (target_tx, mut target_rx) = mpsc::channel::<String>(1);
+  let (config_tx, _) = broadcast::channel(1);
+
+  tokio::spawn({
+    let mut cache = cache.clone();
+    let config_tx = config_tx.clone();
+    async move {
+      let mut targets = HashSet::new();
+      loop {
+        tokio::select! {
+          target = target_rx.recv() => {
+            targets.insert(target.unwrap());
+          }
+          _ = refresh_rx.recv() => {
+            for t in &targets {
+              if let Ok(config) = cache.get(t.clone()).await {
+                config_tx.send((t.clone(), config)).unwrap();
+              }
+            }
+          }
+        }
+      }
+    }
+  });
 
   macro_rules! handle_get_config {
     ($prefix:expr, $tenant:expr, $group:expr, $data_id:expr, $cache:expr) => {{
@@ -91,6 +122,8 @@ async fn start_nacos_adapter() {
     .route(
       "/nacos/v2/cs/config",
       get({
+        let prefix = prefix.clone();
+        let mut cache = cache.clone();
         move |Query(params): Query<HashMap<String, String>>| async move {
           let Some(data_id) = params.get("dataId").filter(|s| s.len() > 0) else {
             return (StatusCode::BAD_REQUEST, DATA_ID_NOT_FOUND_2.to_string());
@@ -122,6 +155,69 @@ async fn start_nacos_adapter() {
         }
       }),
     )
+    .route(
+      "/nacos/v1/cs/configs/listener",
+      post(move |headers: HeaderMap, body: String| {
+        let prefix = prefix.clone();
+        let mut cache = cache.clone();
+        let config_tx = config_tx.clone();
+        async move {
+          if !body.starts_with("Listening-Configs=") {
+            // TODO: checkout the actually nacos response
+            return (StatusCode::BAD_REQUEST, "Bad Request".to_string());
+          }
+
+          let mut update_now = vec![];
+          let mut map = HashMap::new();
+          for (target, md5, raw) in body["Listening-Configs=".len()..].split("%01").map(|s| {
+            let mut parts = s.split("%02");
+            let data_id = parts.next().unwrap();
+            let group = parts.next().unwrap();
+            let md5 = parts.next().unwrap();
+            let tenant = parts.next().unwrap_or("public");
+            let path = format!("{}{}/{}/{}", prefix, tenant, group, data_id);
+            (path, md5, s)
+          }) {
+            // TODO: do this in parallel
+            target_tx.send(target.clone()).await.unwrap();
+            if md5 != cache.get(target.clone()).await.unwrap().md5 {
+              update_now.push(raw);
+            }
+            map.insert(target, (md5, raw));
+          }
+
+          if !update_now.is_empty() {
+            return (StatusCode::OK, update_now.join("%01"));
+          }
+
+          let timeout = headers
+            .get("Long-Pulling-Timeout")
+            .and_then(|s| s.to_str().ok())
+            .and_then(|s| s.parse().ok())
+            .unwrap_or(30000);
+          let timeout = sleep(Duration::from_millis(timeout));
+          tokio::pin!(timeout);
+          let mut config_rx = config_tx.subscribe();
+
+          loop {
+            tokio::select! {
+              _ = &mut timeout => {
+                // timeout, nothing is changed
+                return (StatusCode::OK, "".to_string())
+              }
+              res = config_rx.recv() => {
+                if let Ok((path, config)) = res {
+                  let (md5, raw) = map.get(&path).unwrap();
+                  if md5 != &config.md5 {
+                    return (StatusCode::OK, raw.to_string())
+                  }
+                }
+              }
+            }
+          }
+        }
+      }),
+    )
     .fallback(any(|request: Request<Body>| async move {
       error!(uri = %request.uri().to_string(), "unhandled request");
       (StatusCode::NOT_FOUND, "Not Found".to_string())
@@ -137,7 +233,23 @@ async fn start_nacos_adapter() {
 async fn main() {
   tracing::init_default_subscriber();
 
-  tokio::spawn(start_nacos_adapter());
+  let (refresh_tx, refresh_rx) = mpsc::channel(1);
 
-  Extension::new().run().await.unwrap()
+  tokio::spawn(start_nacos_adapter(refresh_rx));
+
+  lambda_extension::run(service_fn(move |event: LambdaEvent| {
+    let refresh_tx = refresh_tx.clone();
+    async move {
+      match event.next {
+        NextEvent::Shutdown(_e) => {}
+        NextEvent::Invoke(_e) => {
+          refresh_tx.send(()).await.unwrap();
+          // TODO: wait for refresh done?
+        }
+      }
+      Ok(()) as Result<(), Error>
+    }
+  }))
+  .await
+  .unwrap()
 }
