236e471cc7ef984032cb8f04a97203c567a8345b
DiscreteTom <discrete_tom@outlook.com>
2024-09-24 06:48:22 +0000

    chore: optimize code

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use crate::config::{fs::FsConfigProvider, proxy::ProxyConfigProvider};
 use config::{provider::ConfigProvider, target::spawn_target_manager};
 use lambda_extension::{
   service_fn,
-  tracing::{self, debug, trace},
+  tracing::{self, debug},
   Error, LambdaEvent, NextEvent,
 };
 use std::{
@@ -32,6 +32,7 @@ async fn main() -> Result<(), Error> {
   let delay_ms = parse_env("AWS_LAMBDA_NACOS_ADAPTER_DELAY_MS", 10);
   let cooldown_ms = parse_env("AWS_LAMBDA_NACOS_ADAPTER_COOLDOWN_MS", 0);
 
+  // start mock nacos, try proxy mode first, otherwise use fs mode
   let refresh_tx = if let Ok(origin) = env::var("AWS_LAMBDA_NACOS_ADAPTER_ORIGIN_ADDRESS") {
     debug!("AWS_LAMBDA_NACOS_ADAPTER_ORIGIN_ADDRESS={}", origin);
     let cp = ProxyConfigProvider::new(origin);
@@ -44,27 +45,25 @@ async fn main() -> Result<(), Error> {
     start_mock_nacos(port, cp).await?
   };
 
-  let (last_refresh_tx, last_refresh_rx) = watch::channel(Instant::now());
+  // start lambda extension
+  let (last_refresh_setter, last_refresh) = watch::channel(Instant::now());
   lambda_extension::run(service_fn(move |event: LambdaEvent| {
     let refresh_tx = refresh_tx.clone();
-    let last_refresh_tx = last_refresh_tx.clone();
-    let last_refresh_rx = last_refresh_rx.clone();
+    let last_refresh_setter = last_refresh_setter.clone();
+    let last_refresh = last_refresh.clone();
+
     async move {
       match event.next {
         NextEvent::Shutdown(_e) => {}
         NextEvent::Invoke(_e) => {
-          trace!("got next invocation");
-
-          if last_refresh_rx.borrow().elapsed().as_millis() < cooldown_ms {
-            trace!("cooldown not reached");
-            // we don't need to refresh config, just return
+          if last_refresh.borrow().elapsed().as_millis() < cooldown_ms {
+            debug!("cooldown not reached");
+            // no need to refresh config, just return
             return Ok(());
           }
 
-          trace!("cooldown reached");
-          last_refresh_tx
-            .send(Instant::now())
-            .expect("last_refresh_rx should not be dropped");
+          debug!("cooldown reached");
+          last_refresh_setter.send(Instant::now())?;
 
           let (changed_tx, mut changed_rx) = mpsc::channel::<()>(1);
           refresh_tx.send(changed_tx).await?;
@@ -72,12 +71,12 @@ async fn main() -> Result<(), Error> {
           while let Some(()) = changed_rx.recv().await {
             changed = true;
           }
-          // now changed_rx.recv() is None, meaning all changed_tx are dropped and the refresh is done
-          trace!(changed, "refresh done");
+          // now changed_rx.recv() returns None, meaning all changed_tx are dropped and the refresh is done
+          debug!(changed, "refresh done");
 
           // only delay if config changed
           if changed && delay_ms > 0 {
-            trace!("sleeping for {}ms", delay_ms);
+            debug!("sleeping for {}ms", delay_ms);
             sleep(Duration::from_millis(delay_ms)).await;
           }
         }
