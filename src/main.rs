ca4224800156a247a2eeddf1a9229c2354230d11
DiscreteTom <discrete_tom@outlook.com>
2024-09-27 09:31:43 +0000

    feat: support runtime api proxy mode

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod grpc;
 mod http;
 
 use crate::config::{fs::FsConfigProvider, proxy::ProxyConfigProvider};
+use aws_lambda_runtime_proxy::{LambdaRuntimeApiClient, MockLambdaRuntimeApiServer};
 use config::{provider::ConfigProvider, target::spawn_target_manager};
 use lambda_extension::{
   service_fn,
@@ -30,9 +31,11 @@ async fn main() -> Result<(), Error> {
   let cache_size = parse_env("AWS_LAMBDA_NACOS_ADAPTER_CACHE_SIZE", 64);
   let delay_ms = parse_env("AWS_LAMBDA_NACOS_ADAPTER_DELAY_MS", 100);
   let cooldown_ms = parse_env("AWS_LAMBDA_NACOS_ADAPTER_COOLDOWN_MS", 0);
+  let proxy_port = parse_env("AWS_LAMBDA_NACOS_ADAPTER_PROXY_PORT", 0);
 
   // start mock nacos, try proxy mode first, otherwise use fs mode
   let refresh_tx = if let Ok(origin) = env::var("AWS_LAMBDA_NACOS_ADAPTER_ORIGIN_ADDRESS") {
+    // TODO: rename to passthrough mode
     debug!("AWS_LAMBDA_NACOS_ADAPTER_ORIGIN_ADDRESS={}", origin);
     let cp = ProxyConfigProvider::new(cache_size, origin);
     start_mock_nacos(port, cp).await?
@@ -44,8 +47,68 @@ async fn main() -> Result<(), Error> {
     start_mock_nacos(port, cp).await?
   };
 
-  // start lambda extension
   let (last_refresh_setter, last_refresh) = watch::channel(Instant::now());
+
+  // start lambda runtime api proxy
+  if proxy_port != 0 {
+    let cooldown_ms = parse_env("AWS_LAMBDA_NACOS_ADAPTER_PROXY_COOLDOWN_MS", 0);
+    let delay_ms = parse_env("AWS_LAMBDA_NACOS_ADAPTER_PROXY_DELAY_MS", 100);
+    let refresh_tx = refresh_tx.clone();
+    let last_refresh_setter = last_refresh_setter.clone();
+    let last_refresh = last_refresh.clone();
+    tokio::spawn(async move {
+      MockLambdaRuntimeApiServer::bind(proxy_port)
+        .await
+        .serve(move |req| {
+          // TODO: simplify code
+          let refresh_tx = refresh_tx.clone();
+          let last_refresh_setter = last_refresh_setter.clone();
+          let last_refresh = last_refresh.clone();
+          async move {
+            if req.uri().path() != "/2018-06-01/runtime/invocation/next" {
+              // not invocation/next, just forward
+              return LambdaRuntimeApiClient::forward(req).await;
+            }
+
+            // else, the request is invocation/next, forward the request first
+            let res = LambdaRuntimeApiClient::forward(req).await;
+            // now we get the response, we should refresh config before returning the response to the handler
+
+            if last_refresh.borrow().elapsed().as_millis() < cooldown_ms {
+              debug!("cooldown not reached");
+            } else {
+              debug!("cooldown reached");
+              last_refresh_setter
+                .send(Instant::now())
+                .expect("send last_refresh failed");
+
+              let (changed_tx, mut changed_rx) = mpsc::channel::<()>(1);
+              refresh_tx
+                .send(changed_tx)
+                .await
+                .expect("send refresh failed");
+              let mut changed = false;
+              while let Some(()) = changed_rx.recv().await {
+                changed = true;
+              }
+              // now changed_rx.recv() returns None, meaning all changed_tx are dropped and the refresh is done
+              debug!(changed, "refresh done");
+
+              // only delay if config changed
+              if changed && delay_ms > 0 {
+                debug!("sleeping for {}ms", delay_ms);
+                sleep(Duration::from_millis(delay_ms)).await;
+              }
+            }
+
+            res
+          }
+        })
+        .await
+    });
+  }
+
+  // start lambda extension
   lambda_extension::run(service_fn(move |event: LambdaEvent| {
     let refresh_tx = refresh_tx.clone();
     let last_refresh_setter = last_refresh_setter.clone();
@@ -57,6 +120,7 @@ async fn main() -> Result<(), Error> {
           // TODO: print nacos logs? user should provide a file path like /tmp/nacos/logs/nacos/config.log
         }
         NextEvent::Invoke(_e) => {
+          // TODO: don't refresh if the proxy is already refreshed
           if last_refresh.borrow().elapsed().as_millis() < cooldown_ms {
             debug!("cooldown not reached");
             // no need to refresh config, just return
